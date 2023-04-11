ce262f65b4b970b3a61e63ed40f3ca16057b323e
cuongtq1297 <cuongtq1297@gmail.com>
2023-04-11 17:15:00 +0700

    import missing config

---
+++ src/main/java/org/example/ImportEmailMissingConfig.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ImportEmailMissingConfig {
+    public static void importData(String data) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            connection = getConnection.connect();
+            connection.setAutoCommit(false);
+            BufferedReader reader = new BufferedReader(new StringReader(data.trim()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\s+");
+                if(!parts[0].equals("Tapname")){
+                    String importSql = "insert into email.missing_config (tap_name,record,sdr_amount,create_date)" +
+                            " values(?,?,?,now())" ;
+                    ps = connection.prepareStatement(importSql);
+                    ps.setString(1,parts[0]);
+                    ps.setString(2,parts[1]);
+                    ps.setString(3,parts[2]);
+                    ps.executeUpdate();
+                }
+            }
+            connection.commit();
+            connection.close();
+
+            reader.close();
+
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
