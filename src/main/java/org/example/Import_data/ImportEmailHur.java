54fc682dce55eefac0616498fc72f99051e5d518
cuongtq1297 <cuongtq1297@gmail.com>
2023-04-17 21:47:16 +0700

    lam do

--- src/main/java/org/example/Import_data/ImportEmailHur.java
+++ src/main/java/org/example/Import_data/ImportEmailHur.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,6 +18,7 @@ import java.util.Map;
 
 public class ImportEmailHur {
     private static final Logger logger = LogManager.getLogger(ImportEmailHur.class);
+
     public static boolean importData(String data) throws SQLException {
         boolean result = false;
         int resultInsert = 0;
@@ -24,37 +26,57 @@ public class ImportEmailHur {
         String line;
         Connection connection = null;
         PreparedStatement ps = null;
+        PreparedStatement ps1 = null;
+        ResultSet rs = null;
         try {
             connection = GetConnection.connect();
             connection.setAutoCommit(false);
-            String importSql = "INSERT INTO email.hur_email_attachment_data" +
-                    "(sender, recipient, sequence_no, threshold, date_time_of_analysis, date_time_of_report_creation, BEGINNING_OF_THE_OBSERVATION_PERIOD, END_OF_THE_OBSERVATION_PERIOD,create_at)\n" +
-                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?,NOW())";
-            ps = connection.prepareStatement(importSql);
+            String getDataImportConfig = "select fields from email.data_import_config where type = 'HUR'";
+//            String importSql = "INSERT INTO email.hur_email_attachment_data" +
+//                    "(sender, recipient, sequence_no, threshold, date_time_of_analysis, date_time_of_report_creation, BEGINNING_OF_THE_OBSERVATION_PERIOD, END_OF_THE_OBSERVATION_PERIOD,create_at)\n" +
+//                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?,NOW())";
+            ps = connection.prepareStatement(getDataImportConfig);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String fields = rs.getString("fields");
+                String[] fieldNames = fields.split(",");
+                String insertQuery = "INSERT INTO table_name (";
+                for (int i = 0; i < fieldNames.length; i++) {
+                    insertQuery += fieldNames[i];
+                    if (i < fieldNames.length - 1) {
+                        insertQuery += ",";
+                    }
+                }
+                insertQuery += ") VALUES (";
+                for (int i = 0; i < fieldNames.length; i++) {
+                    insertQuery += "?";
+                    if (i < fieldNames.length - 1) {
+                        insertQuery += ",";
+                    }
+                }
+                insertQuery +=")";
+                ps = connection.prepareStatement(insertQuery);
+            }
             Map<String, String> map1 = new HashMap<>();
             Map<String, String> map2 = new HashMap<>();
             Map<String, String> mapMerge = new HashMap<>();
             while ((line = reader.readLine()) != null) {
                 if (line.startsWith("H")) {
                     List<String> fields = Arrays.asList(line.split(","));
-                    map1.put("sender",fields.get(1));
-                    map1.put("recipient",fields.get(2));
-                    map1.put("sequence_no",fields.get(3));
-                    map1.put("threshold",fields.get(4));
-                    map1.put("date_time_of_analysis",fields.get(5));
-                    map1.put("date_time_of_report_creation",fields.get(6));
+                    map1.put("sender", fields.get(1));
+                    map1.put("recipient", fields.get(2));
+                    map1.put("sequence_no", fields.get(3));
+                    map1.put("threshold", fields.get(4));
+                    map1.put("date_time_of_analysis", fields.get(5));
+                    map1.put("date_time_of_report_creation", fields.get(6));
                 } else if (line.startsWith("N")) {
                     List<String> fields = Arrays.asList(line.split(","));
-                    map2.put("BEGINNING_OF_THE_OBSERVATION_PERIOD",fields.get(1));
-                    map2.put("END_OF_THE_OBSERVATION_PERIOD",fields.get(2));
+                    map2.put("BEGINNING_OF_THE_OBSERVATION_PERIOD", fields.get(1));
+                    map2.put("END_OF_THE_OBSERVATION_PERIOD", fields.get(2));
                 }
             }
             // Làm dở
-            mapMerge = mergeMaps(map1,map2);
-            resultInsert =  ps.executeUpdate();
-            if(resultInsert == 1){
-                result = true;
-            }
+            mapMerge = mergeMaps(map1, map2);
             connection.commit();
 
         } catch (SQLException | IOException e) {
@@ -65,6 +87,7 @@ public class ImportEmailHur {
         }
         return result;
     }
+
     public static Map<String, String> mergeMaps(Map<String, String>... maps) {
         Map<String, String> result = new HashMap<>();
         for (Map<String, String> map : maps) {
