e0c8e94af523b263a7ee2677fb5926f1c0dd46e2
cuongtq <cuongtq1297@gmail.com>
2023-04-18 17:16:51 +0700

    fix data hur

--- src/main/java/org/example/Import_data/ImportEmailHur.java
+++ src/main/java/org/example/Import_data/ImportEmailHur.java
@@ -18,12 +18,17 @@ public class ImportEmailHur {
     private static final Logger logger = LogManager.getLogger(ImportEmailHur.class);
 
     public static boolean importData(String data) throws Exception {
+        Connection connection = null;
         boolean result = false;
         BufferedReader reader = new BufferedReader(new StringReader(data));
         String line;
         try {
+            connection = GetConnection.connect();
+            connection.setAutoCommit(false);
             Map<String, String> map1 = new HashMap<>();
             Map<String, String> map2 = new HashMap<>();
+            Map<String, String> map3 = new HashMap<>();
+            Map<String, String> map4 = new HashMap<>();
             Map<String, String> mapMerge = new HashMap<>();
             while ((line = reader.readLine()) != null) {
                 if (line.startsWith("H")) {
@@ -38,12 +43,52 @@ public class ImportEmailHur {
                     List<String> fields = Arrays.asList(line.split(","));
                     map2.put("BEGINNING_OF_THE_OBSERVATION_PERIOD", fields.get(1));
                     map2.put("END_OF_THE_OBSERVATION_PERIOD", fields.get(2));
+                } else if (line.startsWith("P")) {
+                    List<String> fields = Arrays.asList(line.split(","));
+                    map3.put("imsi", fields.get(1));
+                    map3.put("date_first_event", fields.get(2));
+                    map3.put("time_first_event", fields.get(3));
+                    map3.put("date_last_event", fields.get(4));
+                    map3.put("time_last_event", fields.get(5));
+                    map3.put("dc", fields.get(6));
+                    map3.put("nc", fields.get(7));
+                    map3.put("volume", fields.get(8));
+                    map3.put("sdr", fields.get(9));
+                } else if (line.startsWith("C")) {
+                    List<String> fields = Arrays.asList(line.split(","));
+                    map4.put("imsi", fields.get(1));
+                    map4.put("date_first_event", fields.get(2));
+                    map4.put("time_first_event", fields.get(3));
+                    map4.put("date_last_event", fields.get(4));
+                    map4.put("time_last_event", fields.get(5));
+                    map4.put("dc", fields.get(6));
+                    map4.put("nc", fields.get(7));
+                    map4.put("volume", fields.get(8));
+                    map4.put("sdr", fields.get(9));
+                }
+                if (!map3.isEmpty() && line.startsWith("P")) {
+                    mapMerge = mergeMaps(map1, map2, map3);
+                    result = InsertData(connection, mapMerge);
+                    if (!result) {
+                        break;
+                    }
+                }
+                if (!map4.isEmpty() && line.startsWith("C")) {
+                    mapMerge = mergeMaps(map1, map2, map4);
+                    result = InsertData(connection, mapMerge);
+                    if (!result) {
+                        break;
+                    }
                 }
             }
-            mapMerge = mergeMaps(map1, map2);
-            result = InsertData(mapMerge);
+            if (result) {
+                connection.commit();
+            }
+
         } catch (Exception e) {
             logger.error("import data fail" + e);
+        } finally {
+            connection.close();
         }
         return result;
     }
@@ -56,15 +101,12 @@ public class ImportEmailHur {
         return result;
     }
 
-    public static boolean InsertData(Map mapMerge) throws Exception {
+    public static boolean InsertData(Connection connection, Map mapMerge) throws Exception {
         boolean result = false;
         int resultInsert = 0;
-        Connection connection = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         try {
-            connection = GetConnection.connect();
-            connection.setAutoCommit(false);
             String getDataImportConfig = "select fields from email.data_import_config where type = 'HUR'";
             ps = connection.prepareStatement(getDataImportConfig);
             rs = ps.executeQuery();
@@ -99,7 +141,6 @@ public class ImportEmailHur {
         } catch (Exception e) {
             logger.error(e);
         } finally {
-            connection.close();
             ps.close();
             rs.close();
         }
